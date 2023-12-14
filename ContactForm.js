88976883565a3cd4f9959cc1d403e9392a25298c
Sometri OENG <67381317+sometri@users.noreply.github.com>
2023-12-14 19:15:29 +0700

    Create ContactForm.js

---
+++ ContactForm.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Perform contact form submission logic here
+    console.log("Form data submitted:", formData);
+
+    // You can add additional logic, such as sending the data to a server
+    // using an API or handling the form submission in your application.
+  };
+
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" name="name" value={formData.name} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" name="email" value={formData.email} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Subject:
+          <input type="text" name="subject" value={formData.subject} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Message:
+          <textarea name="message" value={formData.message} onChange={handleChange} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
